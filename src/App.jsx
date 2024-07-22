4056311dd33e5c709f317bdbd6f634d4ccedaa4b
Ernst Stolz <eric@spiritsdontfly.com>
2024-07-22 17:05:26 +0200

    add fetching notes from a user

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
     async function getUsers() {
@@ -16,13 +18,36 @@ function App() {
     getUsers();
   }, []);
 
+  useEffect(() => {
+    async function getNotes() {
+      const notes = await axios.get(
+        `https://starter-express-with-postgres-koalas.onrender.com/${selectedUser}/notes`
+      );
+
+      if ("message" in notes.data) {
+        setNotes([]);
+      } else {
+        setNotes(notes.data);
+      }
+    }
+    getNotes();
+  }, [selectedUser]);
+
   return (
     <>
       <h1>Note-taking App</h1>
       <div className="card">
         {users.map((user) => (
-          <button key={user.id}>{user.name}</button>
+          <button key={user.id} onClick={() => setSelectedUser(user.name)}>
+            {user.name}
+          </button>
         ))}
+        <div>Selected: {selectedUser}</div>
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>{note.content}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
