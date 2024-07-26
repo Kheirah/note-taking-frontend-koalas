1503ae97377b64afe69063972d5f2d0c86495dc4
Ernst Stolz <eric@spiritsdontfly.com>
2024-07-26 14:56:04 +0200

    add note to user

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,55 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import "./App.css";
+
+const RenderPostgresURL =
+  "https://starter-express-with-postgres-koalas.onrender.com";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState("");
   const [notes, setNotes] = useState([]);
+  const [backendMessage, setBackendMessage] = useState("");
 
   useEffect(() => {
     async function getUsers() {
-      const users = await axios.get(
-        "https://starter-express-with-postgres-koalas.onrender.com/users"
-      );
+      const users = await axios.get(`${RenderPostgresURL}/users`);
 
       setUsers(users.data);
     }
     getUsers();
   }, []);
 
-  useEffect(() => {
-    async function getNotes() {
-      const notes = await axios.get(
-        `https://starter-express-with-postgres-koalas.onrender.com/${selectedUser}/notes`
-      );
+  const getNotes = useCallback(async () => {
+    const notes = await axios.get(`${RenderPostgresURL}/${selectedUser}/notes`);
 
-      if ("message" in notes.data) {
-        setNotes([]);
-      } else {
-        setNotes(notes.data);
-      }
+    if ("message" in notes.data) {
+      setNotes([]);
+    } else {
+      setNotes(notes.data);
     }
-    getNotes();
   }, [selectedUser]);
+
+  useEffect(() => {
+    getNotes();
+  }, [selectedUser, getNotes]);
+
+  async function handleAddNote(event) {
+    event.preventDefault();
+
+    const content = new FormData(event.target).get("content");
+    const category = new FormData(event.target).get("category");
+
+    const { data } = await axios.post(
+      `${RenderPostgresURL}/${selectedUser}/notes`,
+      { content, category }
+    );
+
+    event.target.reset();
+
+    setBackendMessage(data.message);
+    getNotes();
+  }
 
   return (
     <>
@@ -48,6 +66,14 @@ function App() {
             <li key={note.id}>{note.content}</li>
           ))}
         </ul>
+        <form onSubmit={handleAddNote}>
+          <label htmlFor="note">Note:</label>
+          <input id="note" type="text" name="content" />
+          <label htmlFor="category">Category:</label>
+          <input id="category" type="text" name="category" />
+          <button type="submit">Add note</button>
+        </form>
+        Message from Backend: {backendMessage}
       </div>
     </>
   );
